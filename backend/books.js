78d764305701b6f3f397000f8e386851dff5caf7
Snezhana Dimitrova <snezhana.rosenova@gmail.com>
2022-09-03 16:16:55 +0300

    add nodemon and fake data

---
+++ backend/books.js
@@ -0,0 +1,59 @@
+const books = [
+  {
+    id: 1,
+    title: '1984',
+    author: 'George Orwell',
+    dateOfPublishing: '1961',
+    image: 'https://www.amazon.com/1984-Signet-Classics-George-Orwell/dp/0451524934',
+    description: 'Written more than 70 years ago, 1984 was George Orwell\'s chilling prophecy about the future. And while 1984 has come and gone, his dystopian vision of a government that will do anything to control the narrative is timelier than ever...',
+    pages: 328,
+    price: 7.85,
+    read: false
+  },
+  {
+    id: 2,
+    title: 'Animal Farm',
+    author: 'George Orwell',
+    dateOfPublishing: '2004',
+    image: 'https://images-na.ssl-images-amazon.com/images/I/71wdbkiRokL.jpg',
+    description: 'George Orwell\'s timeless and timely allegorical novel—a scathing satire on a downtrodden society\'s blind march towards totalitarianism.',
+    pages: 140,
+    price: 7.48,
+    read: false
+  },
+  {
+    id: 3,
+    title: 'That Will Never Work',
+    author: 'Marc Randolph',
+    dateOfPublishing: '2019',
+    image: 'https://www.amazon.co.uk/That-Will-Never-Work-Netflix-ebook/dp/B07QRVWBX2',
+    description: 'In the tradition of Phil Knight\'s Shoe Dog comes the incredible untold story of how Netflix went from concept to company - all revealed by co-founder and first CEO Marc Randolph.',
+    pages: '337',
+    price: 20,
+    read: false
+  },
+  {
+    id: 4,
+    title: 'The Airbnb Story',
+    author: 'Leigh Gallagher',
+    dateOfPublishing: '2017',
+    image: 'https://www.amazon.com/Airbnb-Story-Ordinary-Disrupted-Controversy/dp/0544952669',
+    description: 'An investigative behind-the-scenes story of the creation and growth of the highly disruptive start-up Airbnb.',
+    pages: '256',
+    price: 21.40,
+    read: false
+  },
+  {
+    id: 5,
+    title: 'Shoe Dog',
+    author: 'Phil Knight',
+    dateOfPublishing: '2016',
+    image: 'https://www.amazon.com/Shoe-Dog-Memoir-Creator-Nike-ebook/dp/B0176M1A44',
+    description: 'In this instantand tenacious New York Times bestseller, Nike founder and board chairman Phil Knight “offers a rare and revealing look at the notoriously media-shy man behind the swoosh” (Booklist, starred review), illuminating his company’s early days as an intrepid start-up and its evolution into one of the world’s most iconic, game-changing, and profitable brands.',
+    pages: '399',
+    price: 15.95,
+    read: false
+  }
+]
+
+module.exports = books;
--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const books = require('./books');
 
 const app = express();
 const port = 3000;
@@ -10,6 +11,18 @@ app.use((req, res, next) => {
 
 app.get('/', (req, res) => {
   res.send('hello world');
+})
+
+app.get('/api/books', (req, res) => res.json(books));
+
+app.get('/api/books/:id', (req, res) => {
+  const found = books.some((books) => books.id === parseInt(req.params.id));
+
+  if(found) {
+    res.json(books.filter((books) => books.id === parseInt(req.params.id)));
+  } else {
+    res.status(404).send(`No book with the id of ${req.params.id}`);
+  }
 })
 
 app.listen(port, () => {
