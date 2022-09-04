a3d6545509170df7f0cc7b73756f826d237e02eb
Snezhana Dimitrova <snezhana.rosenova@gmail.com>
2022-09-04 18:06:10 +0300

    add put method

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.get('/api/books/:id', (req, res) => {
   const bookId = req.params.id;
   const result = books.find((books) => books.id === parseInt(bookId));
 
-  if(result) {
+  if (result) {
     res.json(result);
   } else {
     sendError(res, 404, `Book with id of ${bookId} not found`);
@@ -35,7 +35,7 @@ app.post('/api/books', (req, res) => {
   ];
 
   const missingProperty = [];
-  for(let property of required) {
+  for (let property of required) {
     if(!data.hasOwnProperty(property)) {
         missingProperty.push(property);
     }
@@ -60,6 +60,50 @@ app.post('/api/books', (req, res) => {
   books.push(newBook);
   res.status(201).json(newBook);
 });
+
+app.put('/api/books/:id', (req, res) => {
+  const data = req.body;
+  const bookId = req.params.id;
+  const currentBook = books.find((books) => books.id === parseInt(bookId));
+
+  if (!currentBook) {
+    return res.sendStatus(204);
+  }
+
+  const required = [
+    'title', 'author', 'dateOfPublishing',
+    'description', 'pages', 'price'
+  ];
+
+  const missingProperty = [];
+  for(let property of required) {
+    if(!data.hasOwnProperty(property)) {
+        missingProperty.push(property);
+    }
+  }
+
+  if(missingProperty.length > 0) {
+    sendError(res, 400, `The ${missingProperty} are missing`);
+  }
+
+  const updatedBook = {
+    id: currentBook.id,
+    title: data.title,
+    author: data.author,
+    dateOfPublishing: data.dateOfPublishing,
+    image: data.image || currentBook.image,
+    description: data.description,
+    pages: data.pages,
+    price: data.price,
+    read: data.read || currentBook.read
+  }
+
+  const index = books.indexOf(currentBook);
+  books[index] = updatedBook;
+
+  res.status(200).json(updatedBook);
+})
+
 
 app.delete('/api/books/:id', (req, res) => {
   const bookId = req.params.id;
